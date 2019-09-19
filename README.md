721023de7487022bd842d2fca27c697c1098c7a0
Yiheng (NUC) <development.henryquan@gmail.com>
2019-09-19 20:25:47 +1000

    stateless widget

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ usage: widget [option] [name]
 -f  create a stateful widget
 -l  create a stateless widget (it is 'L' not '1')
 ```
+
+It is like my [React Native component program](https://github.com/HenryQuan/rncomponent)
--- widget.c
+++ widget.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
+int exists(char*);
 int createStateful(char*);
 int createStateless(char*);
 void usage();
@@ -12,11 +14,41 @@ int main(int argc, char* argv[])
         usage();
         return 1;
     } else {
+        FILE *widget;
+
         char* option = argv[1];
+        char* filename = argv[2];
+        
         if (strcmp(option, "-f") == 0) {
-            printf("Creating stateful widget...\n");
+            printf("Creating stateful widget\n");
+            if (!exists(filename)) {
+                widget = fopen(filename, "w");
+                fprintf(widget, "import 'package:flutter/material.dart';\n\n");
+                fprintf(widget, "/// %s class\n", filename);
+                fprintf(widget, "class %s extends StatelessWidget {\n\n", filename);
+                fprintf(widget, "   %s({Key key}) : super(key: key);\n\n", filename);
+                fprintf(widget, "   @override\n");
+                fprintf(widget, "   Widget build(BuildContext context) {\n");
+                fprintf(widget, "       return Container();\n");
+                fprintf(widget, "   }\n");
+                fprintf(widget, "}\n");
+                fclose(widget);
+            }
         } else if (strcmp(option, "-l") == 0) {
-            printf("Creating stateless widget...\n");
+            printf("Creating stateless widget\n");
+            if (!exists(filename)) {
+                widget = fopen(filename, "w");
+                fprintf(widget, "import 'package:flutter/material.dart';\n\n");
+                fprintf(widget, "/// %s class\n", filename);
+                fprintf(widget, "class %s extends StatelessWidget {\n\n", filename);
+                fprintf(widget, "   %s({Key key}) : super(key: key);\n\n", filename);
+                fprintf(widget, "   @override\n");
+                fprintf(widget, "   Widget build(BuildContext context) {\n");
+                fprintf(widget, "       return Container();\n");
+                fprintf(widget, "   }\n");
+                fprintf(widget, "}\n");
+                fclose(widget);
+            }
         } else {
             // Invalid option
             printf("Invalid option!\n");
@@ -25,6 +57,16 @@ int main(int argc, char* argv[])
     }
 
     return 0;
+}
+
+// Check if file exists
+int exists(char* filename) {
+    if (access(filename, F_OK) != -1) {
+        printf("%s exists", filename);
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 // Create a stateful widget
